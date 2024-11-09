8f4ae9bafade4a93ea994fb14c14bf3f8745e377
GiovaniWHB <giovaniwhb@gmail.com>
2024-11-09 00:17:17 -0300

    fix: Corrigindo a URL da API

--- agendei-mobile/src/constants/api.js
+++ agendei-mobile/src/constants/api.js
@@ -10,7 +10,7 @@ import axios from "axios";
 // Configurar a URL base da API
 const api = axios.create({
     // Toda vez que o app for fazer uma requisição, o começo da URL vai ser essa
-    baseURL: "http://192.168.68.63:3001",
+    baseURL: "http://192.168.0.7:3001",
 });
 
 export default api;
